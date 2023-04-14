878e312faedc95d86e0d2f534696cf54b739a32f
Mark P Thomas <markums@gmail.com>
2023-04-14 12:32:15 -0600

    Readjusted code for better matching w/ optimization case

--- Backtracking/SudokuSolver.js
+++ Backtracking/SudokuSolver.js
@@ -1,3 +1,32 @@
+// T: O(9^(n*n)), For every unassigned index, there are 9 possible options so the time complexity
+// S: O(n*n), for storing board
+// where n = board dimension
+export function solveSudoku(board) {
+  const nextCell = getNextEmptyCell(board);
+
+  // No empty space left, board is complete
+  if (!nextCell) {
+    return board;
+  }
+
+  // Else for each-row backtrack
+  const { row, col } = nextCell;
+
+  for (let i = 0; i < DIGITS.length; i++) {
+    let digit = DIGITS[i];
+    if (isValid(board, nextCell, digit)) {
+      board[row][col] = digit;
+      if (solveSudoku(board)) {
+        return board;
+      } else {
+        board[row][col] = EMPTY;
+      }
+    }
+  }
+
+  return false;
+}
+
 const DIGITS = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const EMPTY = '.';
 
@@ -40,33 +69,4 @@ function isValid(board, nextCell, digit) {
   }
 
   return true;
-}
-
-// T: O(9^(n*n)), For every unassigned index, there are 9 possible options so the time complexity
-// S: O(n*n), for storing board
-// where n = board dimension
-export function solveSudoku(board) {
-  const nextCell = getNextEmptyCell(board);
-
-  // No empty space left, board is complete
-  if (!nextCell) {
-    return board;
-  }
-
-  // Else for each-row backtrack
-  const { row, col } = nextCell;
-
-  for (let i = 0; i < DIGITS.length; i++) {
-    let digit = DIGITS[i];
-    if (isValid(board, nextCell, digit)) {
-      board[row][col] = digit;
-      if (solveSudoku(board)) {
-        return board;
-      } else {
-        board[row][col] = EMPTY;
-      }
-    }
-  }
-
-  return false;
 }
